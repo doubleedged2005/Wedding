c17c7dad71643d46dde74d007c41def1639d8d8e
Ben McMahon <benmcmahon100@gmail.com>
2015-10-11 00:34:56 +0000

    reactgit add -A! from workspace master

---
+++ public/javascripts/Weddings.js
@@ -0,0 +1,59 @@
+var RSVP = React.createClass({
+    componentDidMount: function(){
+      $('form').on('submit',  function(){
+          var data = $(this).serialize();
+          $.ajax({
+              url: "/api/weddingguests",
+              method: "post",
+              dataType: "JSON",
+              data: data
+          }).done(function(data){
+              console.log(data);
+          }); 
+          return false;
+      });
+      
+    },
+    render: function(){
+        return(
+            <form>
+                <div className="form-group">
+                    <label> Name
+                        <input name = "name" className="form-control" />
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label> Email
+                        <input name = "email" className="form-control"/>
+                    </label>
+                </div>    
+                <div className="form-group">
+                    <label> RSVP
+                        <div className="radio">
+                            <label>
+                                <input type="radio" name="rsvpRadio" id="Yes" value="Yes" defaultChecked />
+                                Yes, we can attend! Yay!
+                            </label>
+                        </div>
+                        <div className="radio">
+                            <label>
+                                <input type="radio" name="rsvpRadio" id="No" /> Sadly, we cannot attend. :(
+                            </label>
+                        </div>
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label>Number of Adults (Ages 13+)</label>
+                    <input name = "numOfGuestsOver13" className="form-control" />
+                </div>
+                 <div className="form-group">
+                    <label>Number of Children (Ages 12 and under, but don't worry about including a count for babies</label>
+                    <input name = "numOfGuestsUnder13" className="form-control" />
+                    <input type ="submit" className="btn btn-primary"/>
+                </div>
+            </form>
+        );
+    }
+});
+
+React.render(<RSVP data = {[]} />, document.getElementById('mount'));
--- routes/weddingguests.js
+++ routes/weddingguests.js
@@ -1,9 +1,6 @@
 /**
  * Created by Briana on 10/5/15.
  */
-/**
- * Created by Briana on 10/4/15.
- */
 var express = require('express');
 var router = express.Router();
 
@@ -20,18 +17,18 @@ router.get('/', function(req, res) {
 });
 
 router.post('/', function(req, res){
-    console.log("success");
     var collection = db.get('WeddingGuests');
     collection.insert({
         name: req.body.name,
         email: req.body.email,
-        rsvp: req.body.rsvp,
+        rsvp: req.body.rsvpRadio,
         numOfGuestsOver13: req.body.numOfGuestsOver13,
         numOfGuestsUnder13: req.body.numOfGuestsUnder13
     }, function(err, guest){
         if (err) console.log(err);
 
         res.json(guest);
+        console.log("success");
     });
 });
 
